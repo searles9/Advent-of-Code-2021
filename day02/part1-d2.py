4a1c58a0512c0a6a9b8f305a76acde21faca84a6
searles9 <donovansearles@gmail.com>
2021-12-03 13:11:15 -0500

    completed d2 p1

--- day02/part1-d2.py
+++ day02/part1-d2.py
@@ -1,1 +1,22 @@
 # Day 2 - Part 1
+
+def get_position(file):
+    depth = int(0)
+    horizontal = int(0)
+    with open(file) as file:
+        for line in file:
+            line = line.strip()
+            line = line.split(" ")
+            if line[0].startswith("forward"):
+                horizontal += int(line[1])
+            if line[0].startswith("down"):
+                depth += int(line[1])
+            if line[0].startswith("up"):
+                depth -= int(line[1])
+    return depth * horizontal
+
+
+
+
+if __name__ == "__main__":
+    print(get_position("input-d2.txt"))
---
+++ day02/part2-d2.py
@@ -0,0 +1,4 @@
+# Day 2 - Part 2
+
+if __name__ == "__main__":
+    pass
